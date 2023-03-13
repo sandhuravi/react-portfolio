3306b97be88cd99607d495401eb61fee156de83b
Sandhu <sandhur312@gmail.com>
2023-03-14 03:00:38 +0530

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          I’m a Frontend Software Developer specializing in React, Responsive
+          web design, CSS, HTML, and Javascript. Love to create complete web
+          applications from scratch with backend API systems. I have a hunger
+          for learning new things and am always trying to stretch my abilities.
+        </p>
+
+        <br />
+
+        {/* <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          pariatur, vel similique sint, nobis aspernatur ut praesentium
+          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
+          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
+          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
+          magni quo! Eum cupiditate debitis labore.
+        </p> */}
+      </div>
+    </div>
+  );
+};
+
+export default About;
